df2b18e87870a3cd0847bc90767cff31aa3a4203
Rithik <60679530+Rithik149@users.noreply.github.com>
2023-11-04 23:29:32 +0530

    Delete JavaScript directory

--- JavaScript/Task1/java.js
+++
@@ -1,5 +0,0 @@
-function show(){
-    let dice=Math.floor(Math.random()*6)+1;
-    document.getElementById("myRoll").innerHTML = dice;
-    //document.write(dice);
-}
--- JavaScript/Task2/java.js
+++
@@ -1,6 +0,0 @@
-function sum(){
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
-    let sum=+num1 + +num2;
-    document.getElementById("Sum").innerHTML = sum;
-}
